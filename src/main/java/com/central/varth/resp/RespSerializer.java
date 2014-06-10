31293790538641ad5eddb49a4842a82aee8ddb2c
Faisal Adnan <faisal.adnan@gmail.com>
2014-06-10 14:46:38 +0200

    added serializer

---
+++ src/main/java/com/central/varth/resp/RespSerializer.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ * Copyright 2014 Central Software
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package com.central.varth.resp;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class RespSerializer {
+
+	public RespSerializer() {}
+	
+	public String serialize(String command)
+	{
+		StringBuilder cmd = new StringBuilder();
+		List<String> cmdList = tokenize(command);
+		cmd.append(getHeader(cmdList));
+		cmd.append(getBody(cmdList));
+		return cmd.toString();
+	}
+	
+	public List<String> tokenize(String command)
+	{
+		String[] arr = command.split(" ");
+		return Arrays.asList(arr);
+	}
+	
+	private String getHeader(List<String> cmdList)
+	{
+		StringBuffer sb = new StringBuffer();
+		sb.append(ProtocolConstant.COMMAND_ASTERISK);
+		sb.append(cmdList.size());
+		sb.append(ProtocolConstant.CRLF);
+		return sb.toString();
+	}
+	
+	private String getBody(List<String> cmdList)
+	{
+		StringBuffer sb = new StringBuffer();	
+		for (int i=0;i<cmdList.size();i++)
+		{
+			sb.append(ProtocolConstant.COMMAND_DOLLAR);
+			sb.append(cmdList.get(i).length());
+			sb.append(ProtocolConstant.CRLF);
+			sb.append(cmdList.get(i));
+			sb.append(ProtocolConstant.CRLF);	
+		}	
+		return sb.toString();		
+	}
+	
+}
